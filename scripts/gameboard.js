b52678296f22815d92008b8d24b4f92829595aee
Key <key@HAL2.localdomain>
2026-01-13 22:06:20 -0700

    Write test for receiveAttack function and write that function in the battleship file

--- scripts/gameboard.js
+++ scripts/gameboard.js
@@ -3,6 +3,7 @@ import { createShip } from "./ship.js";
 export function createGameboard() {
     const ROWS = 10;
     const COLUMNS = 10;
+
     const carrier = createShip(5);
     const battleship = createShip(4);
     const destroyer = createShip(3);
@@ -10,40 +11,55 @@ export function createGameboard() {
     const patrolBoat = createShip(2);
     const ships = [carrier, battleship, destroyer, submarine, patrolBoat];
 
-    const gameboard = [];
+    const grid = initializeGrid();
 
-    function printGameboard() {
+
+    function initializeGrid() {
+        let array = [];
         for (let i = 0; i < ROWS; i++) {
+            array[i] = [];
             for (let j = 0; j < COLUMNS; j++) {
-                console.log(`${i}, ${j}: ${gameboard[i][j]}`);
+                array[i][j] = null;
             }
         }
+        return array;
     }
 
-    function initializeGameboard() {
-        for (let i = 0; i < ROWS; i++) {
-            gameboard[i] = [];
-
-            for (let j = 0; j < COLUMNS; j++) {
-                gameboard[i][j] = null;
-            }
+    function placeShip(x, y, ship) {
+        for (let i = y; i < ship.length; i++) {
+            grid[x][i] = ship;
         }
+        return grid[x][y];
     }
-    
+
     function receiveAttack(x, y) {
-        gameboard[0][0] = carrier;
-        if (gameboard[x, y]) {
-            return "Hits: " + gameboard[x][y].hit();
+        if (grid[x][y] === 0 || grid[x][y] === 1) {
+            return -1;
+        }
+        if (grid[x][y]) {
+            grid[x][y].hit();
+            grid[x][y] = 1;
+            return 1;
         }
         else {
-            return "Miss!";
+            grid[x][y] = 0;
+            return 0;
         }
     }
 
-    initializeGameboard();
+    function printGrid() {
+        for (let i = 0; i < ROWS; i++) {
+            for (let j = 0; j < COLUMNS; j++) {
+                console.log(grid[i][j]);
+            }
+        }
+    }
 
     return {
-        receiveAttack
+        ships,
+        placeShip,
+        receiveAttack,
+        printGrid
     }
 
 }
--- scripts/tests/gameboard.test.js
+++ scripts/tests/gameboard.test.js
@@ -2,9 +2,18 @@
     
     describe("gameboard object interface methods", () => {
         const gameboard = createGameboard();
+        const ships = gameboard.ships;
+
+        test("placeShip function places the ships on the correct coordinates", () => {
+            expect(gameboard.placeShip(0, 0, ships[0])).toBe(ships[0]);
+        });
+
+        test("placeShip function places the ships on the correct coordinates", () => {
+            expect(gameboard.placeShip(1, 0, ships[2])).toBe(ships[2]);
+        });
 
         test("receiveAttack method calls the ship.hit function", () => {
-            expect(gameboard.receiveAttack(0, 0)).toBe("Hits: 1");
-        })
+            expect(gameboard.receiveAttack(0, 0)).toBe(1);
+        });
 
     });
